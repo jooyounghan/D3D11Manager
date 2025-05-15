5a92fca9dc61bbbf7375334fb5aa1d3b4dcf59b9
Younghan Joo <rsfnqf98@naver.com>
2025-05-15 09:53:28 +0900

    R32만 사용하는 RenderTarget 생성을 위한 코드 작성

--- D3D11Manager/D3D11Manager/RTVOption.cpp
+++ D3D11Manager/D3D11Manager/RTVOption.cpp
@@ -1,5 +1,9 @@
 #include "RTVOption.h"
+#include "MacroUtilities.h"
 
+#include <unordered_map>
+
+using namespace std;
 using namespace D3D11;
 
 void RTVOption::InitializeByOption(
@@ -8,10 +12,43 @@ void RTVOption::InitializeByOption(
     ID3D11Resource* resource
 )
 {
-	device->CreateRenderTargetView(resource, NULL, &m_rtv);
+	ID3D11Texture2D* texture2D = nullptr;
+	HRESULT hr = resource->QueryInterface(__uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&texture2D));
+
+	if (SUCCEEDED(hr))
+	{
+		D3D11_TEXTURE2D_DESC texture2DDesc;
+		texture2D->GetDesc(&texture2DDesc);
+		D3D11_RENDER_TARGET_VIEW_DESC rtvDesc = GetRenderTargetViewDesc(texture2DDesc);
+		device->CreateRenderTargetView(resource, &rtvDesc, &m_rtv);
+		texture2D->Release();
+	}
+
 }
 
 void RTVOption::Swap(RTVOption& rtvOptionIn)
 {
 	m_rtv.Swap(rtvOptionIn.m_rtv);
+}
+
+D3D11_RENDER_TARGET_VIEW_DESC D3D11::RTVOption::GetRenderTargetViewDesc(const D3D11_TEXTURE2D_DESC& texture2dDesc)
+{
+    D3D11_RENDER_TARGET_VIEW_DESC rtvDesc;
+    ZeroMem(rtvDesc);
+    rtvDesc.Format = GetRenderTargetViewFormatFromTextureFormat(texture2dDesc.Format);
+    rtvDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+    return rtvDesc;
+}
+
+DXGI_FORMAT RTVOption::GetRenderTargetViewFormatFromTextureFormat(const DXGI_FORMAT& textureFormat)
+{
+	static unordered_map<DXGI_FORMAT, DXGI_FORMAT> textureFormatToRTVFormat{
+		{ DXGI_FORMAT_R32_TYPELESS, DXGI_FORMAT_R32_FLOAT },
+	};
+
+	if (textureFormatToRTVFormat.find(textureFormat) != textureFormatToRTVFormat.end())
+	{
+		return textureFormatToRTVFormat[textureFormat];
+	}
+	return textureFormat;
 }
